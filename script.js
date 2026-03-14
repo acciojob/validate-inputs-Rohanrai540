955edb91de8c44918e9f12e0ce2c5258ff36cd1c
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-03-14 23:22:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function validateInput(input, pattern){
+    let regex = new RegExp(pattern);
+
+    if(regex.test(input.value)){
+        input.style.backgroundColor = "lightgreen";
+    }else{
+        input.style.backgroundColor = "pink";
+    }
+}
